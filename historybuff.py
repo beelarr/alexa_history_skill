8ae31569ec4ef221977eab35c5be93d1e8e06fa0
beelarr <bryonl@me.com>
2017-12-29 16:18:28 -0600

    Fixed an error with square brackets

--- historybuff.py
+++ historybuff.py
@@ -1,61 +1,54 @@
 import logging
-
-# enables accessing operation system dependent functionality
 import os
-
-# regular expression matching operations
 import re
-
-#  creates compatibility across python 2 || 3
 from six.moves.urllib.request import urlopen
-
 
 
 from flask import Flask
 from flask_ask import Ask, request, session, question, statement
 
-# creates and instance of Flask and Ask
+
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
-# api url
-URL_PREFIX = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts' \
-             '&format=json&explaintext=&exsectionformat=plain&redirects=&titles='
 
-# number of events to read at a given time
-PAGINATION_SIZE = 2
+# URL prefix to download history content from Wikipedia.
+URL_PREFIX = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts' + \
+    '&format=json&explaintext=&exsectionformat=plain&redirects=&titles='
 
-# length of delimiter between individual events
+# Constant defining number of events to be read at one time.
+PAGINATION_SIZE = 3
+
+# Length of the delimiter between individual events.
 DELIMITER_SIZE = 2
 
-# number of responses from wiki
+# Size of events from Wikipedia response.
 SIZE_OF_EVENTS = 10
 
-# defines session attribute key for event index
+# Constant defining session attribute key for the event index
 SESSION_INDEX = 'index'
 
-# event text for event date
+# Constant defining session attribute key for the event text key for date of events.
 SESSION_TEXT = 'text'
 
 
 @ask.launch
 def launch():
-    speech_output = 'What day do you want events for?'
-    reprompt_text = "You can get historical events for any day of the year. " + \
-                    "For example, you could say today, or December twentieth. " + \
-                    " Which day do you want?"
+    speech_output = 'History buff. What day do you want events for?'
+    reprompt_text = "With History Buff, you can get historical events for any day of the year. " + \
+                    "For example, you could say today, or August thirtieth. " + \
+                    "Now, which day do you want?"
     return question(speech_output).reprompt(reprompt_text)
 
 
-# converts day to date from utterances
-@ask.intent('GetFirstEventIntent', convert={'day': 'date'})
+@ask.intent('GetFirstEventIntent', convert={ 'day': 'date' })
 def get_first_event(day):
     month_name = day.strftime('%B')
     day_number = day.day
     events = _get_json_events_from_wikipedia(month_name, day_number)
     if not events:
-        speech_output = "There is a problem connecting to my brain at this time. Please try again later."
+        speech_output = "There is a problem connecting to Wikipedia at this time. Please try again later."
         return statement('<speak>{}</speak>'.format(speech_output))
     else:
         card_title = "Events on {} {}".format(month_name, day_number)
@@ -66,13 +59,12 @@ def get_first_event(day):
             card_output += "{}\n".format(events[i])
         speech_output += " Wanna go deeper into history?"
         card_output += " Wanna go deeper into history?"
-        reprompt_text = "You can get historical events for any day of the year. " + \
-                        "For example, you could say today, or July first. " + \
-                        "Which day do you want?"
+        reprompt_text = "With History Buff, you can get historical events for any day of the year. " + \
+                        "For example, you could say today, or August thirtieth. " + \
+                        "Now, which day do you want?"
         session.attributes[SESSION_INDEX] = PAGINATION_SIZE
         session.attributes[SESSION_TEXT] = events
         speech_output = '<speak>{}</speak>'.format(speech_output)
-        # support for alexa cards in alexa app
         return question(speech_output).reprompt(reprompt_text).simple_card(card_title, card_output)
 
 
@@ -98,12 +90,12 @@ def get_next_event():
 
 @ask.intent('AMAZON.StopIntent')
 def stop():
-    return statement("Goodbye, for now")
+    return statement("Goodbye")
 
 
 @ask.intent('AMAZON.CancelIntent')
 def cancel():
-    return statement("Goodbye, for now")
+    return statement("Goodbye")
 
 
 @ask.session_ended
@@ -111,7 +103,6 @@ def session_ended():
     return "{}", 200
 
 
-# builds url, opens, reads, decodes and sends that to parse method
 def _get_json_events_from_wikipedia(month, date):
     url = "{}{}_{}".format(URL_PREFIX, month, date)
     data = urlopen(url).read().decode('utf-8')
@@ -136,15 +127,15 @@ def _parse_json(text):
         except ValueError:
             event_text = text[start_index:]
             done = True
-        # replaces dashes in wiki return
+        # replace dashes returned in text from Wikipedia's API
         event_text = event_text.replace('\\u2013', '')
-        # add comma after year so Alexa pauses re.sub is regex substitute, <0>
+        # add comma after year so Alexa pauses before continuing with the sentence
         event_text = re.sub('^\d+', r'\g<0>,', event_text)
         events.append(event_text)
     events.reverse()
     return events
 
-# https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+
 if __name__ == '__main__':
     if 'ASK_VERIFY_REQUESTS' in os.environ:
         verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
