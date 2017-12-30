b978a413ced3251b79dd44595c053b275717061d
beelarr <bryonl@me.com>
2017-12-29 18:20:46 -0600

    Add help intents

--- historyskill.py
+++ historyskill.py
@@ -108,6 +108,12 @@ def cancel():
     return statement("Goodbye, for now")
 
 
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    return statement()
+
+
+
 @ask.session_ended
 def session_ended():
     return "{}", 200
